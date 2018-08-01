c5f84e4a2d42d4e66645265fe45e05bcbf308ae4
Vishal Dhiman <vishal.dhiman@exigent3d.com>
2018-08-01 18:05:44 +0530

    Initial Commit

---
+++ AirWay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirWay
+//
+//  Created by Vishal Dhiman on 8/1/18.
+//  Copyright © 2018 Vishal Dhiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
